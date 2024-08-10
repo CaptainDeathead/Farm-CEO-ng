bae85e6d06848179f3d22681d58f649d3ab05de1
CaptainDeathead <CaptainDeathead@users.noreply.github.com>
2024-08-10 12:57:46 +0800

    Add .spec back

--- game/data.py
+++ game/data.py
@@ -2,5 +2,13 @@
 
 from typing import Tuple
 
+# ---------- PANEL ----------
 PANEL_WIDTH: int = 620
+
+# ---------- GAME ----------
 BACKGROUND_COLOR: Tuple[int, int, int] = (0, 200, 255)
+
+# ---------- CONFIG ----------
+BUILD: bool = False
+SUPPORTED_PLATFORMS: Tuple[str] = ("android_x86_64", "android_v7a", "android_v8a", "Linux", "Windows")
+PLATFORM: str = "android_v8a"
