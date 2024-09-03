63364b33b057b4509796bdc407e491b787287c52
CaptainDeathead <CaptainDeathead@users.noreply.github.com>
2024-09-03 20:42:18 +0800

    Cleanup

---
+++ game/paddock_manager.py
@@ -0,0 +1,114 @@
+import pygame as pg
+
+from resource_manager import SaveManager
+from paddock import Paddock
+from data import *
+
+from typing import Dict, List, Tuple
+
+class PaddockManager:
+    def __init__(self, map_image: pg.Surface, paddocks: Dict[int, any], scale: float) -> None:
+        self.map_image = map_image
+        self.scale = scale
+
+        self.paddocks = self.parse_paddocks(paddocks)
+        self.init_paddocks()
+
+    def parse_paddocks(self, paddocks: Dict[str, any]) -> List[Paddock]:
+        pdk_list = []
+        for paddock in paddocks:
+            pdk_list.append(Paddock(paddocks[paddock], paddock, self.scale))
+        
+        return pdk_list
+
+    def locate_paddock_boundary(self, paddock: Paddock) -> None:
+        def rgb_darker_than(rgb: Tuple[int, int, int], brightness: int) -> bool:
+            if rgb[0] < brightness and rgb[1] < brightness and rgb[2] < brightness: return True
+            return False
+        
+        darkness = 110
+        
+        def get_adj_edge(pos: Tuple[int, int]) -> Tuple[int, int]:
+            for x in range(-1, 2):
+                for y in range(-1, 2):
+                    if x == 0 and y == 0: continue
+
+                    nx, ny = pos[0] + x, pos[1] + y
+                    if not rgb_darker_than(self.map_image.get_at((nx, ny)), darkness): return (nx, ny)
+            
+            return (-1, -1)
+        
+        if DEBUG_BOUNDARY_LOADING: pg.display.update(pg.display.get_surface().blit(self.map_image, (0, 0)))
+
+        boundary = []
+
+        # Starting at center
+        cx, cy = paddock.center
+
+        start_x = cx
+        start_y = 0
+
+        # find boundary start from center
+        y = cy
+        while 1:
+            if not rgb_darker_than(self.map_image.get_at((cx, y)), darkness):
+                start_y = y
+                break
+            y += 1
+        
+        # trace boundary
+        curr_x = start_x
+        curr_y = start_y
+
+        while 1:
+            if DEBUG_BOUNDARY_LOADING: pg.display.update(pg.draw.circle(pg.display.get_surface(), (255, 255, 255), (curr_x, curr_y), 1))
+            found_new_px = False
+
+            # get adjacent paddock squares (black)
+            for x in range(-1, 2):
+                if found_new_px: continue
+                
+                for y in range(-1, 2):
+                    if (x == 0 and y == 0) or found_new_px: continue
+                    
+                    nx, ny = curr_x + x, curr_y + y
+
+                    if (nx, ny) == (start_x, start_y): # Back to start (Done)
+                        if len(boundary) < 2: continue
+
+                        paddock.set_boundary(boundary)
+                        return
+                    
+                    if (nx, ny) in boundary: continue
+
+                    if rgb_darker_than(self.map_image.get_at((nx, ny)), darkness):
+                        edge = get_adj_edge((nx, ny))
+
+                        if edge != (-1, -1):
+                            found_new_px = True
+                            boundary.append((nx, ny))
+                            curr_x, curr_y = nx, ny
+                        else:
+                            if DEBUG_BOUNDARY_LOADING: pg.display.update(pg.draw.circle(pg.display.get_surface(), (255, 0, 0), (nx, ny), 1))
+
+            if DEBUG_BOUNDARY_LOADING: pg.display.update(pg.draw.circle(pg.display.get_surface(), (255, 0, 0), (nx, ny), 1))
+
+    def fill_paddock(self, paddock: Paddock, color: pg.Color) -> None:
+        boundary = paddock.boundary
+        pg.draw.polygon(self.map_image, color, boundary)
+
+    def load_paddock_state(self, paddock: Paddock) -> None:
+        self.fill_paddock(paddock, STATE_COLORS[paddock.state])
+
+    def set_paddock_state(self, paddock_number: int, state: int) -> None:
+        self.paddocks[paddock_number-1].state = state
+        self.load_paddock_state(self.paddocks[paddock_number-1])
+
+    def init_paddocks(self) -> None:
+        for paddock in self.paddocks:
+            self.locate_paddock_boundary(paddock)
+            self.load_paddock_state(paddock)
+
+        save_manager = SaveManager({})
+        save_manager.set_paddocks(self.paddocks)
+        save_manager.save_game()
